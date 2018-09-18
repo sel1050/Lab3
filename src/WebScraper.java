c5e143bb0beda90b2c3a31b8d6e1ac58bc705234
sel1050 <42983452+sel1050@users.noreply.github.com>
2018-09-18 18:40:09 -0500

    Second Project Commit

---
+++ src/WebScraper.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
+/**
+ * Retrieve contents from a URL and return them as a string.
+ *
+ * @param url url to retrieve contents from
+ * @return the contents from the url as a string, or an empty string on error
+ */
+public class WebScraper {
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+    }
+    public static void main(String[] words) {
+        System.out.println(http://erdani.com/tdpl/hamlet.txt);
+    }
+    boolean flag = false;
+    for (int i = 0; i < String[].length; i++) {
+            if (String[].length[i] = "") {
+            i++;
+        }
+    }
+}
+
+
+
+
+
+
